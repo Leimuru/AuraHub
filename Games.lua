05545ece12b8c6e57b5992b5e8f1288116ca26fb
Sunken <114361572+sunkenball@users.noreply.github.com>
2023-02-17 22:30:51 +0000

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -2,7 +2,7 @@ return {
   [{11746859781}] = { -- Bubble Gum Clicker
     ["Soggyware"] = [[loadstring(game:HttpGet("https://www.soggy-ware.cf"))()]],
   },
-  [{9872472334}] = {
+  [{9872472334}] = { -- Evade
     ["Roflux"] = [[loadstring(game:HttpGet("https://roflux.net/scripts/games/9872472334.lua"))()]]
   }
 }
