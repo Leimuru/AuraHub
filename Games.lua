68e2f6f9d25c1d62a9d014c8dfe79225c3615e7e
Sunken <114361572+sunkenball@users.noreply.github.com>
2023-02-17 21:32:41 +0000

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,5 +1,9 @@
 return {
-  [{11746859781,}] = {
-    ["Soggyware"] = [[loadstring(game:HttpGet("https://www.soggy-ware.cf"))()]]
+  [{11746859781,}] = { -- Bubble Gum Clicker
+    ["Soggyware"] = [[loadstring(game:HttpGet("https://www.soggy-ware.cf"))()]],
+    ["Soggyware2"] = [[loadstring(game:HttpGet("https://www.soggy-ware.cf"))()]],
+  },
+  [{1512,}] = {
+    ["Soggyware"] = [[loadstring(game:HttpGet("https://www.soggy-ware.cf"))()]],
   },
 }
