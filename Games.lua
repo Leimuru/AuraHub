eb5db51b742a7885079794c7b61c437e5be62e9a
Sunken <114361572+sunkenball@users.noreply.github.com>
2023-02-17 22:30:34 +0000

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,9 +1,8 @@
 return {
-  [{11746859781,}] = { -- Bubble Gum Clicker
-    ["Soggyware"] = [[loadstring(game:HttpGet("https://www.soggy-ware.cf"))()]],
-    ["Soggyware2"] = [[loadstring(game:HttpGet("https://www.soggy-ware.cf"))()]],
-  },
-  [{1512,}] = {
+  [{11746859781}] = { -- Bubble Gum Clicker
     ["Soggyware"] = [[loadstring(game:HttpGet("https://www.soggy-ware.cf"))()]],
   },
+  [{9872472334}] = {
+    ["Roflux"] = [[loadstring(game:HttpGet("https://roflux.net/scripts/games/9872472334.lua"))()]]
+  }
 }
