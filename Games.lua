f17849f8c492667126a2d6bed6484650058c7f47
Sunken <114361572+sunkenball@users.noreply.github.com>
2023-02-17 22:37:06 +0000

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,8 +1,40 @@
 return {
-  [{11746859781}] = { -- Bubble Gum Clicker
-    ["Soggyware"] = [[loadstring(game:HttpGet("https://www.soggy-ware.cf"))()]],
-  },
-  [{9872472334}] = { -- Evade
-    ["Roflux"] = [[loadstring(game:HttpGet("https://roflux.net/scripts/games/9872472334.lua"))()]]
-  }
+    [{11746859781}] = { -- Bubble Gum Clicker
+        ["Soggyware"] = "https://www.soggy-ware.cf",
+    },
+    [{9872472334}] = { -- Evade
+        ["Roflux"] = "https://roflux.net/scripts/games/9872472334.lua",
+        ["Strew"] = "https://raw.githubusercontent.com/9Strew/roblox/main/gamescripts/evade.lua"
+    },
+    [{6284583030}] = { -- Pet Simulator X
+        ["Cipex"] = "https://raw.githubusercontent.com/cipex-rbx/cipex/main/loader.lua",
+        ["Zerox"] = "https://raw.githubusercontent.com/RunDTM/ZeeroxHub/main/Loader.lua",
+        ["Cloud"] = "https://raw.githubusercontent.com/BadScripters/CloudHub/main/hub"
+    },
+    [{2753915549}] = { -- Blox Fruits
+        ["Neva Hub"] = "https://raw.githubusercontent.com/VEZ2/NEVAHUB/main/2",
+        ["Mukoro"] = "https://raw.githubusercontent.com/xDepressionx/Free-Script/main/AllScript.lua",
+        ["Mango"] = "https://gitlab.com/L1ZOT/mango-hub/-/raw/main/Mango-Bloxf-Fruits-Beta",
+        ["Tawan"] = "https://raw.githubusercontent.com/kill55547/TAWAN_HUB/main/hub.lua.txt",
+        ["Tuper"] = "https://raw.githubusercontent.com/TUPERX/TUPERX/Main/MenuBF/README.md",
+        ["Blaze X"] = "https://raw.githubusercontent.com/WindowsXp12/BlxzeRBLX/main/BLxzeHub.lua",
+        ["HOHO"] = "https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI",
+        ["Zen Hub"] = "https://raw.githubusercontent.com/Kaizenofficiall/ZenHub/main/Loader"
+    },
+    [{11156779721}] = { -- The Survival Game
+        ["KYA-WARE"] = "https://raw.githubusercontent.com/qxkya/Kya-Ware/main/TheSurvivalGame.lua"
+    },
+    [{11606818992}] = { -- Steep Steps
+        ["Autofarm"] = "https://raw.githubusercontent.com/kodescripts/scripts/main/Flight.lua",
+        ["Legend Handles"] = "https://raw.githubusercontent.com/LOLking123456/Steep1/main/Steps121"
+    },
+    [{2809202155}] = { -- YBA
+        ["Autofarm"] = "https://raw.githubusercontent.com/zakater5/LuaRepo/main/YBA/v3.lua"
+    },
+    [{6516141723,  6839171747}] = {
+        ["Ominous"] = "https://raw.githubusercontent.com/OminousVibes-Exploit/Scripts/main/doors/loadstring.lua"
+    },
+    [{6403373529}] = {
+        ["Autofarm"] = "https://raw.githubusercontent.com/dizyhvh/slap_battles_gui/main/0.lua"
+    }
 }
