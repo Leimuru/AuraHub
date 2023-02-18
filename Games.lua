56379f03917953109afe5c6bf52b11053bf08460
Sunken <114361572+sunkenball@users.noreply.github.com>
2023-02-18 02:17:25 +0000

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -3,5 +3,69 @@ return {
         -- Bubble Gum Clicker
         ["Soggyware"] = "https://www.soggy-ware.cf"
     },
-    
+    [{9872472334}] = {
+        -- Evade
+        ["Roflux"] = "https://roflux.net/scripts/games/9872472334.lua",
+        ["Strew"] = "https://raw.githubusercontent.com/9Strew/roblox/main/gamescripts/evade.lua"
+    },
+    [{6284583030}] = {
+        -- Pet Simulator X
+        ["Cipex"] = "https://raw.githubusercontent.com/cipex-rbx/cipex/main/loader.lua",
+        ["Zerox"] = "https://raw.githubusercontent.com/RunDTM/ZeeroxHub/main/Loader.lua",
+        ["Cloud"] = "https://raw.githubusercontent.com/BadScripters/CloudHub/main/hub"
+    },
+    [{2753915549}] = {
+        -- Blox Fruits
+        ["Neva Hub"] = "https://raw.githubusercontent.com/VEZ2/NEVAHUB/main/2",
+        ["Mukoro"] = "https://raw.githubusercontent.com/xDepressionx/Free-Script/main/AllScript.lua",
+        ["Mango"] = "https://gitlab.com/L1ZOT/mango-hub/-/raw/main/Mango-Bloxf-Fruits-Beta",
+        ["Tawan"] = "https://raw.githubusercontent.com/kill55547/TAWAN_HUB/main/hub.lua.txt",
+        ["Tuper"] = "https://raw.githubusercontent.com/TUPERX/TUPERX/Main/MenuBF/README.md",
+        ["Blaze X"] = "https://raw.githubusercontent.com/WindowsXp12/BlxzeRBLX/main/BLxzeHub.lua",
+        ["HOHO"] = "https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI",
+        ["Zen Hub"] = "https://raw.githubusercontent.com/Kaizenofficiall/ZenHub/main/Loader"
+    },
+    [{11156779721}] = {
+        -- The Survival Game
+        ["KYA-WARE"] = "https://raw.githubusercontent.com/qxkya/Kya-Ware/main/TheSurvivalGame.lua"
+    },
+    [{11606818992}] = {
+        -- Steep Steps
+        ["Autofarm"] = "https://raw.githubusercontent.com/kodescripts/scripts/main/Flight.lua",
+        ["Legend Handles"] = "https://raw.githubusercontent.com/LOLking123456/Steep1/main/Steps121"
+    },
+    [{2809202155}] = {
+        -- YBA
+        ["Autofarm"] = "https://raw.githubusercontent.com/zakater5/LuaRepo/main/YBA/v3.lua"
+    },
+    [{6516141723, 6839171747}] = {
+        --Doors
+        ["Ominous"] = "https://raw.githubusercontent.com/OminousVibes-Exploit/Scripts/main/doors/loadstring.lua"
+    },
+    [{6403373529}] = {
+        -- Slap Battles
+        ["Autofarm"] = "https://raw.githubusercontent.com/dizyhvh/slap_battles_gui/main/0.lua"
+    },
+    [{8737602449}] = {
+        --- Driving Empire
+        ["Windy"] = "https://raw.githubusercontent.com/WindyKung/WindyWare/main/MainScript/DrivingEmpire.lua",
+        ["Soggyware"] = "https://www.soggy-ware.cf"
+    },
+    [{6735572261}] = {
+        --- Pilgrammed
+        ["Load Script"] = "https://raw.githubusercontent.com/LOLking123456/chain1/main/saw2",
+        ["Best"] = "https://raw.githubusercontent.com/DeIivery/roblox/main/pilgrammed"
+    },
+    [{6735572261}] = {
+        --- Shindo
+        ["VG"] = "https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub"
+    },
+    [{6735572261}] = {
+        --- Piece Adventures Simulator
+        ["Pa1n"] = "https://raw.githubusercontent.com/xpa1n/Scripts/main/Piece%20Adventures%20Simulator.lua"
+    },
+    [{6735572261}] = {
+        --- Murder Mystery 2
+        ["Kidachi"] = "https://kidachi.xyz/Script"
+    }
 }
